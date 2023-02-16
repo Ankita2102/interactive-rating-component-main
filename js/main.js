787d60f74957c90509e52a91051f2fc1a88370a2
Ankita Gupta <90860794+Ankita2102@users.noreply.github.com>
2023-02-16 11:22:32 +0530

    my first solution

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// $(function(){
+//     document
+//         .getElementById("myBtn")
+//         .addEventListener("click", function(){
+//         redirect(this.getAttribute("data-page"));        
+//     });
+    
+//     $('button:nth-child(1)')
+//         .click(function(){
+//         redirect($(this).attr('data-page'));
+//     });
+// })
+
+// function redirect(url){
+//     console.log(url);
+//     window.location = file:///E:/interactive-rating-component-main/interactive-rating-component-main/index1.html;
+// }
+
+document.getElementById("myBtn").onclick = function () {
+    window.location.href = "file:///E:/interactive-rating-component-main/interactive-rating-component-main/index1.html";
+};
